5b621d7494ee2a4267fa9921deae08dbe10ad098
Haris Reyaz Wani <119967082+Haris0955@users.noreply.github.com>
2023-01-02 11:57:41 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+
+public class MyClass {
+
+	public  void MyMethod() 
+	{
+		System.out.println("I am in my method");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+		System.out.println("I Am Haris");
+		MyClass m= new MyClass();
+		m.MyMethod();
+	}
+
+}
